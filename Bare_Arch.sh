33afd7a6f8e384940e47a549ebecfb1506d28ac0
HackMEAny <hackme.any@protonmail.com>
2021-08-31 01:17:53 +0530

    Bare install & KDE(GUI) added

---
+++ Bare_Arch.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | 123
+
+# Xorg is installed in GUI part/ check WM or Display Manager
+
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils nfs-utils dnsutils bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call virt-manager qemu qemu-arch-extra iptables-nft ipset firewalld flatpak acpid os-prober ntfs-3g terminus-font packagekit-qt5
+
+# pacman -S --noconfirm xf86-video-intel
+# pacman -S --noconfirm xf86-video-amdgpu
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+# systemctl enable bluetooth
+# systemctl enable cups.service
+systemctl enable sshd
+# systemctl enable avahi-daemon
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable libvirtd
+# systemctl enable firewalld # If you want to enable firewall then uncomment this
+systemctl enable acpid
+
+useradd -m hackme
+echo hackme:password | 123
+usermod -aG libvirt hackme
+
+echo "hackme ALL=(ALL) ALL" >> /etc/sudoers.d/hackme
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
+
+
+
