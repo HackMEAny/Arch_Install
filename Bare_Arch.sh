c744173c0b4916df2ed1ed6e2a1e2b936df5e1a5
HackMEAny <hackme.any@protonmail.com>
2021-11-07 21:01:24 +0530

    locale.gen fixed & password fixed

--- Bare_Arch.sh
+++ Bare_Arch.sh
@@ -2,14 +2,14 @@
 
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
-sed -i '177s/.//' /etc/locale.gen
+sed -i '/^#en_US.UTF-8* /s/^#//' /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "arch" >> /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-echo root:password | 123
+echo "LANG=en_US.UTF-8" >>/etc/locale.conf
+echo "arch" >>/etc/hostname
+echo "127.0.0.1 localhost" >>/etc/hosts
+echo "::1       localhost" >>/etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >>/etc/hosts
+echo root:password | chpasswd
 
 # Xorg is installed in GUI part/ check WM or Display Manager
 
@@ -35,14 +35,9 @@ systemctl enable libvirtd
 systemctl enable acpid
 
 useradd -m hackme
-echo hackme:password | 123
+echo hackme:password | chpasswd
 usermod -aG libvirt hackme
 
-echo "hackme ALL=(ALL) ALL" >> /etc/sudoers.d/hackme
-
+echo "hackme ALL=(ALL) ALL" >>/etc/sudoers.d/hackme
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
-
-
-
-
